0a0870ebc9716a72904e986b834867ff07c2d7ba
Joao Pedro <joaopebordignon@gmail.com>
2025-06-07 16:45:23 -0300

    Ajustando os projeto para funcionar jo java 21, e criando a main

---
+++ po23s-cch-cripto/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import po23s.http.ClienteHttp;
+
+public class Main {
+    public static void main(String[] args) {
+        ClienteHttp teste = new ClienteHttp();
+
+        String resultado = teste.buscaDados("https://www.mercadobitcoin.net/api/ETH/ticker");
+
+        System.out.println(resultado);
+    }
+}
