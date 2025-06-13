6adb76f633e3ebc64a08646c365fe8ce8e33f2e5
Joao Pedro <joaopebordignon@gmail.com>
2025-06-13 19:05:03 -0300

    slicer adicionado

---
+++ po23s-cch-cripto/src/main/java/po23s/model/Slicer.java
@@ -0,0 +1,40 @@
+package po23s.model;
+
+
+import org.json.JSONObject;
+
+public class Slicer {
+
+    static private JSONObject Jsoncovert(String json){
+        JSONObject obj = new JSONObject(json);
+        obj = obj.getJSONObject("ticker");
+        return obj;
+    }
+
+    static public String JsonGetNome(String json){
+        JSONObject cortar = Jsoncovert(json);
+        String parte  = cortar.getString("pair");
+
+        String cortado = parte.substring(3, 6);
+
+        return cortado;
+
+    }
+
+    static public String JsonGetCompra(String json){
+        JSONObject compra = Jsoncovert(json);
+        String valor  = compra.getString("buy");
+        return valor;
+    }
+
+    
+    static public String JsonGetVenda(String json){
+        JSONObject venda = Jsoncovert(json);
+        String valor  = venda.getString("sell");
+
+        return valor;
+
+    }
+}
+
+
