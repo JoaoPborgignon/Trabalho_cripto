574bfdf632c83686b92a5f6adcbe5b300a510391
Joao Pedro <joaopebordignon@gmail.com>
2025-06-26 10:34:38 -0300

    main limpa

--- po23s-cch-cripto/src/main/java/Po23sCchCripto.java
+++ po23s-cch-cripto/src/main/java/Po23sCchCripto.java
@@ -30,25 +30,6 @@ public class Po23sCchCripto {
         }
     });
 
-    try {
-        for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-            if ("Nimbus".equals(info.getName())) {
-                javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                break;
-            }
-        }
-    } catch (ClassNotFoundException ex) {
-        java.util.logging.Logger.getLogger(popup_atualizar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-    } catch (InstantiationException ex) {
-        java.util.logging.Logger.getLogger(popup_atualizar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-    } catch (IllegalAccessException ex) {
-        java.util.logging.Logger.getLogger(popup_atualizar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-    } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-        java.util.logging.Logger.getLogger(popup_atualizar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-    }
-    //</editor-fold>
-
-    /* Create and display the dialog */
     }
 }
 
